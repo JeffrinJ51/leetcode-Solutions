3342cfd2ee7ac37d59239345058f3d12475f6591
JeffrinJ51 <163719549+JeffrinJ51@users.noreply.github.com>
2026-01-18 20:47:16 +0530

    Time: 14 ms (91.38%) | Memory: 43.5 MB (74.85%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,8 +3,8 @@ class Solution {
         if (s == null || s.length() < 1) return "";
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
-            int len1 = expandFromCenter(s, i, i);     // Odd-length
-            int len2 = expandFromCenter(s, i, i + 1); // Even-length
+            int len1 = expandFromCenter(s, i, i);     
+            int len2 = expandFromCenter(s, i, i + 1); 
             int len = Math.max(len1, len2);
             if (len > end - start) {
                 start = i - (len - 1) / 2;
